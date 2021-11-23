33873d40a0bc587dd4c3f65c2b25b46df2b15839
Dominic Potis <potisd22@hsnet.ahsd.org>
2021-11-23 11:40:49 -0500

    Initial commit

---
+++ app/src/main/java/com/potis/honorsmobileapps/quiz/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.potis.honorsmobileapps.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.potis.honorsmobileapps.quiz.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        val rootView= binding.root
+        setContentView(rootView)
+    }
+}
